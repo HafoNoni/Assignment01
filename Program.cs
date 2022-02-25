289cd9bcd5544119cfe8d5645f8d0a54ec6a85d1
HafoNoni <84912019+HafoNoni@users.noreply.github.com>
2022-02-25 08:21:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            double b = 4.0;
+            string c = "polyilaro";
+
+            Console.WriteLine("Enter Second Number:");
+            int second_num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Value:");
+            double second_value = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second String Value:  ");
+            string second_string = Console.ReadLine();
+
+            Console.WriteLine(a + second_num);
+            Console.WriteLine(b + second_value);
+            Console.WriteLine(c + " " + second_string);
+
+            Console.ReadLine();
+        }
+    }
+}
